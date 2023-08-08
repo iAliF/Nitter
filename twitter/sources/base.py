124c25dbc34ddce65832c1ac78808e68fd3b4c5f
Ali Fotouhi <mr.ali.fotouhi@gmail.com>
2023-08-08 17:57:06 +0330

    Update Base class

--- twitter/sources/base.py
+++ twitter/sources/base.py
@@ -1,13 +1,34 @@
 from abc import abstractmethod
+from typing import Dict, Any
+
+import requests
+from requests import RequestException
 
 from ..models import MediaResult
 
 
 class SourceBase:
-    def __init__(self, source_name: str, base_url: str) -> None:
+    def __init__(self, source_name: str, base_url: str, timeout=10, proxies=None) -> None:
         self.source_name = source_name
+        self.timeout = timeout
+        self.proxies = proxies
+
+        self._session = requests.Session()
         self._base_url = base_url
 
     @abstractmethod
-    def get_medias(self) -> MediaResult:
+    def get_medias(self, username: str) -> MediaResult:
         pass
+
+    def _make_request(self, path: str, params: Dict[str, Any] = None) -> str:
+        print(f"{self._base_url}/{path}")
+        try:
+            req = self._session.get(
+                f"{self._base_url}/{path}",
+                params=params,
+                timeout=self.timeout,
+                proxies=self.proxies
+            )
+            return req.text
+        except RequestException as e:
+            print(e)
